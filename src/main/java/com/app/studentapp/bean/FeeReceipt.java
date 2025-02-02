fef4ed0dbb4e1cdef28d694c3246b8cf00931983
poojita <poojitakkr@gmail.com>
2025-02-02 15:04:29 +0530

    resolving issues

--- src/main/java/com/app/studentapp/bean/FeeReceipt.java
+++ src/main/java/com/app/studentapp/bean/FeeReceipt.java
@@ -1,11 +1,11 @@
 package com.app.studentapp.bean;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import jakarta.persistence.*;
-import lombok.Getter;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
--- src/main/java/com/app/studentapp/bean/Teacher.java
+++ src/main/java/com/app/studentapp/bean/Teacher.java
@@ -2,10 +2,11 @@ package com.app.studentapp.bean;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import javax.persistence.*;
 
 @Entity
 @Getter
